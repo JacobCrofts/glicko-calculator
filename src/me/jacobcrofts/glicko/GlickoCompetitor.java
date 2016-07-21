8daf25b53a4fb36fc609b263f18a229803e5a5d3
John Doe <johndoe@example.com>
2016-07-21 16:18:55 -0500

    Fix bug: MatchSet#getCompetitors does not account for repetition

--- src/me/jacobcrofts/glicko/GlickoCompetitor.java
+++ src/me/jacobcrofts/glicko/GlickoCompetitor.java
@@ -1,12 +1,8 @@
 package me.jacobcrofts.glicko;
 
+import java.util.ArrayList;
+
 public final class GlickoCompetitor {
-	
-	private final String id;
-	private final int originalRating;
-	private int newRating;
-	private final double originalRatingDeviation;
-	private double newRatingDeviation;
 	
 	private static double c = 35;
 	
@@ -14,17 +10,62 @@ public final class GlickoCompetitor {
 		GlickoCompetitor.c = c;
 	}
 	
+	private final String id;
+	private final int originalRating;
+	private int newRating;
+	private final double originalRatingDeviation;
+	private double newRatingDeviation;
+	private MatchProfile profile;
+	
 	public GlickoCompetitor(String id, int rating, double oldRatingDeviation, int timeSinceLastParticipation) {
 		this.id = id;
 		this.originalRating = rating;
 		this.originalRatingDeviation = Math.min(350, Math.sqrt(Math.pow(oldRatingDeviation, 2) + Math.pow(c, 2) * timeSinceLastParticipation));
+		this.profile = new MatchProfile();
 	}
 	
 	public GlickoCompetitor(String id, int rating, double oldRatingDeviation) {
 		this.id = id;
 		this.originalRating = rating;
 		this.originalRatingDeviation = oldRatingDeviation;
+		this.profile = new MatchProfile();
 	}
+	
+	public class MatchProfile {
+		
+		private ArrayList<GlickoCompetitor> opponents;
+		private ArrayList<Double> outcomes;
+		
+		private MatchProfile() {
+			this.opponents = new ArrayList<GlickoCompetitor>();
+			this.outcomes = new ArrayList<Double>();
+		}
+		
+		public void registerMatch(GlickoMatch match) {
+			this.opponents.add(match.getOpponent(GlickoCompetitor.this));
+			this.outcomes.add(match.getOutcome(GlickoCompetitor.this).getValue());
+		}
+		
+		public ArrayList<GlickoCompetitor> getOpponents() {
+			return this.opponents;
+		}
+		
+		public ArrayList<Double> getOutcomes() {
+			return this.outcomes;
+		}
+		
+	}
+	
+	public MatchProfile getProfile() {
+		return this.profile;
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	public String getID() {
 		return this.id;
--- src/me/jacobcrofts/glicko/GlickoMatchSet.java
+++ src/me/jacobcrofts/glicko/GlickoMatchSet.java
@@ -1,16 +1,13 @@
 package me.jacobcrofts.glicko;
 
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
+import java.util.Set;
 
 public final class GlickoMatchSet {
-	
-	private static final double q = Math.log(10) / 400;
-	
+		
 	private List<GlickoMatch> matches;
 	
 	public GlickoMatchSet() {
@@ -19,118 +16,24 @@ public final class GlickoMatchSet {
 	
 	public void addMatch(GlickoMatch match) {
 		this.matches.add(match);
+		match.register();
 	}
 	
-	public int getNewRating(GlickoCompetitor player) {
-		
-		int rating = player.getOriginalRating();
-		double deviation = player.getOriginalRatingDeviation();
-		
-		ArrayList<Double> outcomes = new ArrayList<Double>();
-		ArrayList<GlickoCompetitor> opponentData = new ArrayList<GlickoCompetitor>();
-//		LinkedHashMap<Integer, Double> opponentData = new LinkedHashMap<Integer, Double>();
-		
+	private Set<GlickoCompetitor> getCompetitors() {
+//		List<GlickoCompetitor> competitors = new ArrayList<GlickoCompetitor>();
+		Map<GlickoCompetitor, Boolean> competitors = new HashMap<GlickoCompetitor, Boolean>();
 		for (GlickoMatch match : this.matches) {
-			if (match.involves(player)) {
-				outcomes.add(match.getOutcome(player).getValue());
-				GlickoCompetitor opponent = match.getOpponent(player);
-				opponentData.add(opponent);
-//				opponentData.put(opponent.getOriginalRating(), opponent.getOriginalRatingDeviation());
-			}
+			competitors.put(match.getFirstPlayer(), true);
+			competitors.put(match.getSecondPlayer(), true);
 		}
-		
-		return this.getNewRating(rating, deviation, opponentData, outcomes);
-		
-	}
-	
-	public double getNewRatingDeviation(GlickoCompetitor player) {
-		
-		int rating = player.getOriginalRating();
-		double deviation = player.getOriginalRatingDeviation();
-		
-		ArrayList<GlickoCompetitor> opponentData = new ArrayList<GlickoCompetitor>();
-		
-		for (GlickoMatch match : this.matches) {
-			if (match.involves(player)) {
-				GlickoCompetitor opponent = match.getOpponent(player);
-				opponentData.add(opponent);
-			}
-		}
-		
-		return this.getNewRatingDeviation(rating, deviation, opponentData);
-		
+		return competitors.keySet();
 	}
 	
 	public void assignNewDataToAllCompetitors() {		
 		for (GlickoCompetitor player : this.getCompetitors()) {
-			player.setRating(this.getNewRating(player));
-			player.setRatingDeviation(this.getNewRatingDeviation(player));
+			player.setRating(GlickoCalculator.getNewRating(player));
+			player.setRatingDeviation(GlickoCalculator.getNewRatingDeviation(player));
 		}
-	}
-	
-	private List<GlickoCompetitor> getCompetitors() {
-		List<GlickoCompetitor> competitors = new ArrayList<GlickoCompetitor>();
-		for (GlickoMatch match : this.matches) {
-			competitors.add(match.getFirstPlayer());
-			competitors.add(match.getSecondPlayer());
-		}
-		return competitors;
-	}
-	
-	private double getNewRatingDeviation(int rating, double ratingDeviation, ArrayList<GlickoCompetitor> opponents) {
-		return Math.sqrt(1 / ((1 / Math.pow(ratingDeviation, 2)) + (1 / dSquared(rating, opponents))));
-	}
-	
-	private int getNewRating(int originalRating, double ratingDeviation, ArrayList<GlickoCompetitor> opponentData, ArrayList<Double> outcomes) {
-		
-//		System.out.println(opponentData.size() + " : " + outcomes.size());
-		
-		if (opponentData.size() != outcomes.size()) {
-			throw new IllegalArgumentException("Invalid opponent data passed to Glicko calculator.");
-		}
-		
-		for (double outcome : outcomes) {
-			if (outcome != 1 && outcome != 0.5 && outcome != 0) {
-				throw new IllegalArgumentException("Invalid outcome data passed to Glicko calculator.");
-			}
-		}
-		
-		double sum = 0;
-		
-		Iterator<GlickoCompetitor> opponentIterator = opponentData.iterator();
-		Iterator<Double> outcomeIterator = outcomes.iterator();
-		
-		while (opponentIterator.hasNext() && outcomeIterator.hasNext()) {
-			GlickoCompetitor opponent = opponentIterator.next();
-			double outcome = outcomeIterator.next();
-			sum += (g(opponent.getOriginalRatingDeviation()) * (outcome - E(originalRating, opponent.getOriginalRating(), opponent.getOriginalRatingDeviation())));
-		}
-		
-		double prefix = q / ((1 / Math.pow(ratingDeviation, 2)) + (1 / (dSquared(originalRating, opponentData))));
-		
-		return (originalRating + Math.round((float) (prefix * sum)));
-	}
-	
-	private double dSquared(int rating, ArrayList<GlickoCompetitor> opponents) {
-		double sum = 0;
-		
-		Iterator<GlickoCompetitor> it = opponents.iterator();
-		while (it.hasNext()) {
-			GlickoCompetitor opponent = it.next();
-			double g = g(opponent.getOriginalRatingDeviation());
-			double e = E(rating, opponent.getOriginalRating(), opponent.getOriginalRatingDeviation());
-			sum += g * g * e * (1 - e);
-		}
-		
-		return 1 / (q * q * sum);
-	}
-	
-	private double E(int playerRating, int opponentRating, double opponentRatingDeviation) {
-		return 1 / (1 + Math.pow(10, g(opponentRatingDeviation) * (playerRating - opponentRating) / -400));
-	}
-	
-	private double g(double ratingDeviation) {
-		return 1 / (Math.sqrt(1 + (3 * q * q * ratingDeviation * ratingDeviation) / (Math.PI * Math.PI)));
 	}
 
 }
